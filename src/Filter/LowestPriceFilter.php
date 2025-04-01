ef9c6fd0c00f34f925844c167f51eb6f8dcc5f50
VadimST04 <vadikstarchak04@gmail.com>
2025-04-01 22:43:17 +0200

    update

---
+++ src/Filter/LowestPriceFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filter;
+
+use App\DTO\PromotionEnquiryInterface;
+use App\Entity\Promotion;
+
+class LowestPriceFilter implements PromotionsFilterInterface
+{
+
+    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
+    {
+        $enquiry->setDiscountedPrice(50);
+        $enquiry->setPrice(100);
+        $enquiry->setPromotionId(3);
+        $enquiry->setPromotionName('Black Friday half price sale');
+
+        return $enquiry;
+    }
+}
